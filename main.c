fa832921c6d1ccdc4744ba74b006158e3fa111ac
ねこがくれ <156896971+nekogakure@users.noreply.github.com>
2025-10-01 16:02:24 +0900

    aa

---
+++ main.c
@@ -0,0 +1,19 @@
+/**
+ *  このプログラムは、私の親がPCの利用時間をチェックするために、カレンダーに利用時間を書いているのですが、
+ *  私がPCを使い始めるときにまぁ私は記入をわすれます。ので、PCを使い始めるときにお知らせするプログラムです。
+ *
+ *  (c) 2025 nekogakure.
+ *  このプログラムの著作権はnekogakureに帰属しますが、クレジット表記なしでの使用、改変、再配布を許可します。
+ *  このプログラムは無保証で提供されます。使用に伴ういかなる損害についても責任を負いません。
+ *  商用利用、個人利用、非商用利用に問わず自由に使用できます。（誰が使うねん）
+ */
+
+#include <windows.h>
+#include <locale.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nShowCmd) {
+    setlocale(LC_ALL, "JA");
+    MessageBoxW(NULL, L"開始時間かけ！", L"書けよ！カレンダーに！", MB_OK | MB_ICONINFORMATION);
+    return 0;
+}
